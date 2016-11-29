9b17ba18c6a150fd0b6fd7a644441ee8ef86c947
Ildiko Czeller <czeildi@gmail.com>
2016-11-29 07:33:33 +0100

    install shiny, data.table and ggplot2 w packrat

---
+++ libraries.R
@@ -0,0 +1,8 @@
+# these packages were installed with packrat
+library('shiny')
+library('data.table')
+library('ggplot2')
+
+# these packages are present as dependencies, no need to install separately
+library('magrittr')
+library('stringr')

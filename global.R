daa90ce9c54eeccec314ebff5fd69a65eb6089ba
Ildiko Czeller <czeildi@gmail.com>
2016-12-06 21:49:56 +0100

    use lubridate

--- global.R
+++ global.R
@@ -1,2 +1,3 @@
 source('libraries.R')
+source('src/project_values.R')
 invisible(sapply(list.files('src', full.names = TRUE), source))
--- libraries.R
+++ libraries.R
@@ -2,6 +2,8 @@
 library('shiny') # 0.14.2 
 library('data.table') # 1.9.8
 library('ggplot2') # 2.2.0
+library('WDI') # 2.4
+library('lubridate') # 1.6.0
 
 # these packages are present as dependencies, no need to install separately
 library('magrittr') # 1.5

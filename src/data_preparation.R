bab2f4a3607a8fbacacfb406bc7b24d3800cd239
Ildiko Czeller <czeildi@gmail.com>
2016-12-06 22:00:27 +0100

    refactor pull base wdi data

--- src/data_preparation.R
+++ src/data_preparation.R
@@ -1,33 +1,40 @@
-pullBaseWdiData <- function(metric_name) {
-    metric_indicators <- c(
+pullBaseWdiData <- function(series_name) {
+    # series: WDI_data[['series']]
+    
+    series_indicators <- c(
         'population'            = 'SP.POP.TOTL',
         'life_expectancy'       = 'SP.DYN.LE00.IN',
         'GDP_per_capita_income' = 'NY.GDP.PCAP.PP.CD',
         'births_per_woman'      = 'SP.DYN.TFRT.IN'
     )
     
-    if (! metric_name %in% names(metric_indicators)) {
+    if (! series_name %in% names(series_indicators)) {
         stop(
-            'data for this metric is not available, choose one of ',
-            str_c(names(metric_indicators), collapse = ', '),
+            'data for this series is not available, choose one of ',
+            str_c(names(series_indicators), collapse = ', '),
             call. = FALSE
         )
     }
     
     if (!dir.exists('data')) {dir.create('data')}
-    file_name <- file.path('data', str_c(metric_name, '.rds'))
+    file_name <- file.path('data', str_c(series_name, '.rds'))
     
     if (file.exists(file_name)) {
         readRDS(file_name)
     } else {
         WDI(
-            indicator = metric_indicators[[metric_name]],
+            indicator = series_indicators[[series_name]],
             country = 'all',
             start = CONST[['wdi_start_year']],
             end = year(Sys.Date())
         ) %>% 
             data.table() %>% 
-            setnames(metric_indicators[[metric_name]], metric_name) %T>% 
+            setnames(series_indicators[[series_name]], series_name) %T>% 
             saveRDS(file_name)
     }
+}
+
+filterAggregateCountries <- function(dt) {
+    # countries: WDI_data[['country']]
+    # where region == 'Aggregates'
 }

0d996750d353e12b4ea57cfd5026620b59bf933f
Ildiko Czeller <czeildi@gmail.com>
2016-12-10 18:21:02 +0100

    simplify data peeker

--- src/data_peeker.R
+++ src/data_peeker.R
@@ -1,7 +1,6 @@
 dataPeekerUI <- function(id) {
   ns <- NS(id)
   tagList(
-    h3(textOutput(ns('default'))),
     fluidRow(
       column(6, seriesSelector(id)),
       column(6, uiOutput(ns('country_selector')))
@@ -10,7 +9,9 @@ dataPeekerUI <- function(id) {
   )
 }
 
+
 dataPeeker <- function(input, output, session, min_year) {
+  
   output$raw_data <- renderDataTable({
     
     data() %>%
@@ -18,10 +19,6 @@ dataPeeker <- function(input, output, session, min_year) {
       .[order(get(input$series_name))]
     
   }, options = list(pageLength = 5, lengthMenu = c(5,25,50,100,200)))
-  
-  output$default <- renderText({
-    str_c('The default value is: ', str_replace(session$ns(''), '-', ''))
-  })
   
   output$country_selector <- renderUI({
     ns <- session$ns
